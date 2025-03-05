9e04928ce6e0539938474f394f5b6786779ff70d
Nirav <nirav.butani@cron-labs.com>
2025-03-05 16:23:08 +0530

    updated pop

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CommonService } from 'src/app/services/common.service';
 
@@ -14,7 +15,7 @@ export class SignupComponent {
   hideConfirmPassword = true;
   signup_error = null;
 
-  constructor(private fb: FormBuilder,private signupservice: CommonService,private router: Router ) {
+  constructor(private fb: FormBuilder,private signupservice: CommonService,private router: Router,private snackBar: MatSnackBar ) {
     this.signupForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
@@ -34,6 +35,13 @@ export class SignupComponent {
       let body =  this.signupForm.value;
       this.signupservice.signup(body).subscribe((data)=>{
         console.log(data);
+        if(data?.message){
+          this.snackBar.open(data?.message, 'Close', {
+            duration: 10000, // Closes after 3 seconds
+            verticalPosition: 'top',
+            horizontalPosition: 'center', panelClass: ['custom-snackbar']});
+        }
+        
         this.router.navigate(['/auth/login']); 
       },(err)=>{
         this.signup_error = err.error? err?.error:"";
--- src/app/interceptors/jwt.interceptor.ts
+++ src/app/interceptors/jwt.interceptor.ts
@@ -21,18 +21,18 @@ export class JwtInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = this.authService.getToken();
-    console.log(token);
+   
     if (token) {
       request = request.clone({
         setHeaders: { Authorization: `Bearer ${token}` },
         withCredentials: true
       });
-      console.log(request);
+    
     }
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-       console.log(error);
+      
          if(error.status==403){
           this.router.navigate(['/auth/login']);
         
